8dd708fb3aad2cb951674409e2d26cd8096cbb5e
Parveen Bhadoo <35890959+ParveenBhadooOfficial@users.noreply.github.com>
2019-02-28 14:31:30 +0530

    Add files via upload

---
+++ js/www.hashhackers.com_custom.js
@@ -0,0 +1,3 @@
+$(".content-card p:nth-of-type(3)").after('<p><ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-5435553179213419" data-ad-slot="2455920374"></ins></p>'); [].forEach.call(document.querySelectorAll('.adsbygoogle'), function() {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    });
