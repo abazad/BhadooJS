7188f971ec7372aeee868f962bce33f4dddb3ad8
Parveen Bhadoo <35890959+ParveenBhadooOfficial@users.noreply.github.com>
2019-03-01 14:23:12 +0530

    Delete www.hashhackers.com-register.js

--- service_workers/www.hashhackers.com-register.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-	navigator.serviceWorker.register(bhadoo_sw.url)
-	.then(function(registration) { console.log('Hash Hackers Service worker ready'); registration.update(); })
-	.catch(function(error) { console.log('Registration failed with ' + error); });
-  });
-}
