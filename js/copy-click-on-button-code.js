134ea18f34529ce405b0386c48bed5c65221a463
Parveen Bhadoo <35890959+ParveenBhadooOfficial@users.noreply.github.com>
2019-03-01 14:23:58 +0530

    Delete copy-click-on-button-code.js

--- js/copy-click-on-button-code.js
+++
@@ -1,8 +0,0 @@
-function copyToClipboard(text) {
-  var inp = document.createElement('input');
-  document.body.appendChild(inp);
-  inp.value = text;
-  inp.select();
-  document.execCommand('copy', false);
-  inp.remove();
-}
