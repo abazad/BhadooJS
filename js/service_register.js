6d72ec09d994c224b8abbf50a03b69643244fb15
Parveen Bhadoo <35890959+ParveenBhadooOfficial@users.noreply.github.com>
2019-03-01 17:13:50 +0530

    Create service_register.js

---
+++ js/service_register.js
@@ -0,0 +1,1 @@
+if("serviceWorker"in navigator){window.addEventListener("load",function(){navigator.serviceWorker.register(bhadoo_sw.url).then(function(registration){console.log("Hash Hackers Service worker ready");registration.update()}).catch(function(error){console.log("Registration failed with "+error)})})}
