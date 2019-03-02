fe626ed3b8fde6102544fbe51cff55f18fbff376
Parveen Bhadoo <35890959+ParveenBhadooOfficial@users.noreply.github.com>
2019-03-02 15:43:59 +0530

    Create password.js

---
+++ js/password.js
@@ -0,0 +1,29 @@
+	"use strict";
+	function loadPage(pwd) {
+		var hash= pwd;
+		hash= Sha1.hash(pwd);
+		var url= hash + "/";
+		$.ajax({
+			url : url,
+			dataType : "html",
+			success : function(data) {
+				window.location= url;
+			},
+			error : function(xhr, ajaxOptions, thrownError) {
+				parent.location.hash= hash;
+				//$("#wrongPassword").show();
+				$("#password").attr("placeholder","wrong password");
+				$("#password").val("");
+			}
+		});
+	}
+	$("#loginbutton").on("click", function() {
+		loadPage($("#password").val());
+	});
+	$("#password").keypress(function(e) {
+		if (e.which == 13) {
+
+			loadPage($("#password").val());
+		}
+	});
+	$("#password").focus();
