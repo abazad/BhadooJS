3aada690c2e3506315e27c5e79b04465b1dc478f
Parveen Bhadoo <35890959+ParveenBhadooOfficial@users.noreply.github.com>
2019-04-17 14:08:47 +0530

    Create loading.js

---
+++ js/loading.js
@@ -0,0 +1,12 @@
+$(".btn-primary").click(function() {
+    $('.loader-wrapper-').toggleClass('loader-wrapper');
+});
+$(".btn-primary").click(function() {
+    $('.loader-').toggleClass('loader');
+});
+$(".btn-primary").click(function() {
+    $('.section-left-').toggleClass('section-left');
+});
+$(".btn-primary").click(function() {
+    $('.section-right-').toggleClass('section-right');
+});
