8e4af634a3359937d6b8adef664218b92551a094
Parveen Bhadoo <bhadoo@pm.me>
2020-05-05 14:59:29 +0530

    Update one.js

--- api/one.js
+++ api/one.js
@@ -1,6 +1,1 @@
 var apis = ["https://one.driveindex.ga", "https://botone.ve.workers.dev"];
-var server = apis[Math.floor(Math.random() * apis.length)];
-document.getElementById("permaurl").innerHTML =
-    "<a href=" + "'" + window.location.pathname + "'" + ">" + "https://" + window.location.hostname + window.location.pathname + "</a>";
-document.getElementById("tempurl").innerHTML =
-    "<a href=" + "'" + server + window.location.pathname + "'" + ">" + server + window.location.pathname + "</a>";
