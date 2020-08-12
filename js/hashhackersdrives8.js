40093d0e7a319efee8c040b8b21440bf47a2696d
Parveen Bhadoo <bhadoo@pm.me>
2020-08-12 16:29:55 +0530

    Update and rename hashhackersdrives7.js to hashhackersdrives8.js

--- js/hashhackersdrives8.js
+++ js/hashhackersdrives8.js
@@ -1,7 +1,7 @@
 // Load the necessary static in the head and code from https://github.com/ParveenBhadooOfficial/Bhadoo-Drive-Index
 document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/mdui@0.4.3/dist/css/mdui.min.css">');
 // markdown支持
-document.write('<script src="//cdn.jsdelivr.net/npm/markdown-it@10.0.0/dist/markdown-it.min.js"></script>');
+document.write('<script src="//cdn.jsdelivr.net/npm/markdown-it@10.0.0/dist/markdown-it.min.js"></script><script src="https://cdn.jsdelivr.net/gh/ParveenBhadooOfficial/BhadooJS@1.0.10/js/copy-click-on-button-code.js" integrity="sha256-DN8Abv8u8YT3P4PCX9xXKK9agjfXnh8l96HDJrvfMdw=" crossorigin="anonymous"></script>');
 document.write('<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:0 1pc 0 0}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>a:last-child,.mdui-toolbar>i:first-child{display:block}}</style>');
 // add custome theme and darkmode
 if (UI.dark_mode) {
@@ -198,9 +198,9 @@ function requestSearch(params, resultCallback) {
 function list(path) {
   var content = `
 	<br><label class="mdui-textfield-label" style="color:white;">Shareable Link </label><div class="mdui-textfield" id="sharelink"></div>
-	<script>
-	var shareurl = "https://one.driveindex.ga" + window.location.pathname;
-	document.getElementById("sharelink").innerHTML= '<input class="mdui-textfield-input" type="text" value="' + shareurl +'" disabled="" />';
+  <script>
+	var shareurl = 'https://one.driveindex.ga' + window.location.pathname;
+	document.getElementById('sharelink').innerHTML= "<input class='mdui-textfield-input' type='text' value='" + shareurl + "\/' disabled='' /><button class='mdui-btn mdui-color-theme-accent mdui-ripple' onClick='copyToClipboard('" + shareurl + "\/');'>Copy Link</button>";
 	</script>
 	<div id="head_md" class="mdui-typo" style="display:none;padding: 20px 0;"></div>
 
